551d750a805c9aadd3d92a6f1435ca70d90b4fb6
finn-01 <nguyensam576@gmail.com>
2022-02-16 23:24:26 +0700

    set up

---
+++ src/test/java/vn/finn/spring/demojpawithmysql/DemoJpaWithMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.finn.spring.demojpawithmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJpaWithMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
